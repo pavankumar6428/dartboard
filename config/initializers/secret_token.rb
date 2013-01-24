0d63fd2a6a3534deeac36e1213d8f3f9bf7f260f
pavankumar6428 <pavan.kumar@strata.co.in>
2013-01-24 18:01:13 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dartboard::Application.config.secret_token = 'acd92c67bf0a0b1cbf69a1817d61426bf5997aa5481a9849fa69842c961b71b6b558df532cdb89f4510b4fbb8ad58281d467799e045dc96803c572c477989f99'
